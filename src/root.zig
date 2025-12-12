8473303eeb9ec5dd59db61eb557cde1f09575644
subo2002-Laptop <subocashell@gmail.com>
2025-12-12 17:58:28 +0000

    corrected Vector2 declaration in root and added the FP stuff

--- src/root.zig
+++ src/root.zig
@@ -2,4 +2,8 @@ const Vectors = @import("vector.zig");
 
 pub const Vector2B = Vectors.Vector2B;
 pub const Vector2I = Vectors.Vector2I;
-pub const Vector = Vectors.Vector2;
+pub const Vector2 = Vectors.Vector2;
+
+const FPLib = @import("fixed_point.zig");
+pub const FP = FPLib.FP;
+pub const Vector2FP = FPLib.Vector2FP;
